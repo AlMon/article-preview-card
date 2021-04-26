166837e26fde9d0ad0cc17d63d203e972fc186d2
Al Mondragón <al.drago@live.com>
2021-04-25 23:46:53 -0500

    Add aditional styles

--- pages/index.js
+++ pages/index.js
@@ -8,12 +8,12 @@ export default function Home() {
         <link rel='icon' href='/favicon.png' />
       </Head>
 
-      <main className='flex flex-wrap items-center justify-center min-h-screen font-medium bg-[#ecf2f8] overflow-hidden text-[13px] antialiased'>
-        <div className='flex bg-white max-w-[733px] shadow-xl rounded-lg overflow-hidden'>
+      <main className='flex items-center justify-center min-h-screen font-medium bg-[#ecf2f8] overflow-hidden text-[13px] antialiased'>
+        <div className='sm:flex grid mx-8 my-16 bg-white max-w-[733px] shadow-xl rounded-lg overflow-hidden'>
           <img
             src='/drawers.jpg'
             alt='Drawers'
-            className='min-h-[281px] max-w-[286px] object-cover'
+            className='sm:min-h-[281px] sm:max-w-[286px] object-cover'
           />
           <div className='px-10 py-8'>
             <h1 className='text-[20px] font-bold text-[#48556a]'>
@@ -35,8 +35,15 @@ export default function Home() {
                 <h3 className='text-[#48556a] font-bold'>Michelle Appleton</h3>
                 <p className='text-[#9eafc2] mt-1 font-bold'>28 Jun 2020</p>
               </div>
-              <div className='absolute right-0 bg-[#ecf2f8] rounded-full p-[9px] flex items-center justify-center cursor-pointer'>
-                <img src='/icon-share.svg' alt='Share Icon' />
+              <div className='absolute right-0 bg-[#ecf2f8] rounded-full p-[9px] flex items-center justify-center cursor-pointer active:bg-[#48556a]'>
+                <svg
+                  xmlns='http://www.w3.org/2000/svg'
+                  width={15}
+                  height={13}
+                  className='share'
+                >
+                  <path d='M15 6.495L8.766.014V3.88H7.441C3.33 3.88 0 7.039 0 10.936v2.049l.589-.612C2.59 10.294 5.422 9.11 8.39 9.11h.375v3.867L15 6.495z' />
+                </svg>
               </div>
             </div>
           </div>
